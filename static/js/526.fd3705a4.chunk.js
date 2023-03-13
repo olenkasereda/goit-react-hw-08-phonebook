a5466098f08204beabaac0e46046fc71f89d3910
olenkasereda <106010289+olenkasereda@users.noreply.github.com>
2023-03-13 19:46:26 +0000

    Deploying to gh-pages from @ olenkasereda/goit-react-hw-08-phonebook@0600a0b2357092775dc497d67c89987bf9ce76b2 🚀

---
+++ static/js/526.fd3705a4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[526],{4526:function(e,n,t){t.r(n),t.d(n,{default:function(){return f}});var o,r=t(168),a=t(931).Z.h1(o||(o=(0,r.Z)(["\n  display: flex;\n  justify-content: center;\n  padding: 20px;\n  font-size: 48px;\n  font-weight: 700;\n  font-family: 'Montserrat', sans-serif;\n\n  color: #2ee59d;\n"]))),c=t(3329);function f(){return(0,c.jsx)(a,{children:'"Welcome to Phonebook \ud83d\ude0a" '})}}}]);
+//# sourceMappingURL=526.fd3705a4.chunk.js.map
